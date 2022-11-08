fab5451201fee9a0c7b164362d2d207b53a17d50
AnaCAvila <ana.avila@mail.mcgill.ca>
2022-11-08 17:12:33 -0500

    removed rows with dubious values

--- amazon_model.R
+++ amazon_model.R
@@ -296,14 +296,27 @@ tavg = tavg[,-c(1:2)]
 # 500 regrowth
 # 600 secondary suppression
 
+regrowth2 = regrowth[rownames(regrowth) %in% rownames(amazon), ] 
+regrowth2[regrowth2 < 400 & regrowth2 >= 300] = 1 #secondary
+regrowth2[100 < regrowth2 & regrowth2 < 200] = 0 #anthropic
+
+
+tmp = cbind(regrowth2[,3:(ncol(regrowth)-3)],NA)
+tmp2 = cbind(NA, regrowth2[,3:(ncol(regrowth)-3)])
+res = apply(tmp-tmp2, 1, function(x) {any(x == -1)})
+res[is.na(res)] = FALSE
+
+tst = regrowth2[res,]
+
+
 # within these, find 2019-year of last pixel flagged as "regrowth" -> this will give forest age.
-regrowth_instances = sapply(apply(regrowth[3:(ncol(regrowth)-4)], 1, function(x) which(600 > x & x >= 500)),names) # returns rowname/index as well as years flagged as regrowth events
+regrowth_instances = sapply(apply(tst[3:(ncol(tst)-4)], 1, function(x) which(600 > x & x >= 500)),names) # returns rowname/index as well as years flagged as regrowth events
 suppression_instances = sapply(apply(regrowth[3:(ncol(regrowth)-4)], 1, function(x) which(600 <= x & x < 700)),names) # returns rowname/index as well as years flagged as regrowth events
 
 last_regrowth = lapply(lapply(regrowth_instances, unlist), max) # select for only the most recent observed regrowth
 last_regrowth_df = as.data.frame(unlist(last_regrowth))
 last_regrowth_df = as.data.frame(lapply(last_regrowth_df,as.numeric))
-last_regrowth_df = cbind(regrowth[(ncol(regrowth)-2):ncol(regrowth)],last_regrowth_df)
+last_regrowth_df = cbind(regrowth[(ncol(tst)-2):ncol(tst)],last_regrowth_df)
 
 suppression_instances = lapply(lapply(suppression_instances, unlist), max) # select for only the most recent observed regrowth
 suppression_instances_df = as.data.frame(unlist(suppression_instances))
@@ -316,26 +329,6 @@ amazon = cbind(amazon[,1:3], 'forest_age' = 2019-amazon[,4]) # since AGB data is
 
 #############################
 
-regrowth2 = regrowth[rownames(regrowth) %in% rownames(amazon), ] 
-regrowth_instances = sapply(apply(regrowth[3:(ncol(regrowth)-4)], 1, function(x) which(600 > x & x >= 500)),names) # returns rowname/index as well as years flagged as regrowth events
-
-tst = lapply(regrowth2, 1, function(x)(c(x+NA)-c(x-NA) == 2) )
-
-
-
-
-
-ranges = list()
-for (i in 1:length(last_regrowth)){
-  tst = colnames(regrowth)[as.integer(colnames(regrowth)) > as.integer(last_regrowth[[i]])]
-  tst = tst[!is.na(tst)]
-  ranges = cbind(ranges,tst)
-  print(i)
-}
-
-
-head(regrowth[ranges])
-# why are the values for regrowth category all these different values?
 
 
 
