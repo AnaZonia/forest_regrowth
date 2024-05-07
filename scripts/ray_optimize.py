83d8c47d831cc2994b246df8eb47afaef4871b91
AnaCAvila <aavila@mail.mcgill.ca>
2024-05-06 20:18:12 -0400

    restricting histories

--- scripts/ray_optimize.py
+++ scripts/ray_optimize.py
@@ -12,10 +12,11 @@ Author
 
 import os
 import numpy as np
-from scipy.stats import norm
-from ray import tune
 import pandas as pd
-import numpy as np
+from scipy.optimize import minimize
+from scipy.stats import norm
+import ray
+from ray import tune
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -34,7 +35,7 @@ def import_data(path:str):
     data = data.drop(columns=['system:index', '.geo', 'biome'])
 
     # Convert 'soil' and 'ecoreg' to categorical data
-    categorical = ['soil', 'ecoreg', 'indig', 'protec']
+    categorical = ['soil', 'ecoreg']
     data[categorical] = data[categorical].astype('category')
     
     # Create dummy variables for 'ecoreg' and 'soil'
@@ -73,20 +74,30 @@ def growth_curve(pars, data, years):
         for unique_var in non_climatic:
             k += pars[unique_var] * data[unique_var]
 
-    return pars['B0'] + pars['A'] * (1 - np.exp(-k))**pars['theta']
+    return pars['B0'] + pars['A'] * (1 - np.exp(-pars['last_fire']*data['last_fire']))**pars['theta']
 
 
 def negative_log_likelihood(pars, data):
     # Assuming a normal distribution for the data
-    sigma = 1 # You might need to adjust this based on your data
-    likelihood = norm.pdf(data['agbd'], loc=model_output, scale=sigma)
+    likelihood = norm.pdf(data['agbd'] - growth_curve(pars, data, years), loc=0, scale=pars['sd'])
     return -np.sum(np.log(likelihood))
+
+x0 = list(pars.values())
+
+
+# Now you can call minimize with the modified growth_curve function
+result = minimize(negative_log_likelihood, x0, args=(data,), method='Nelder-Mead')
+
+
 
 space = {
     'B0': tune.uniform(0, 100),
     'A': tune.uniform(0, 500),
     'theta': tune.uniform(0, 1),
+    'sd': tune.uniform(0, 1),
 }
+
+space.update({key: tune.uniform(0, 100) for key in pars})
 
 # Initialize Ray
 ray.init()
@@ -95,16 +106,22 @@ ray.init()
 def objective(config):
     return negative_log_likelihood(config, data)
 
-def run_model(path):
-    data = import_data(path)  # Replace "path_to_your_data.csv" with the actual path to your CSV file
+# def run_model(path):
+#     data = import_data(path)  # Replace "path_to_your_data.csv" with the actual path to your CSV file
 
-# Run the optimization
-analysis = run(
+def short_trial_dirname_creator(trial):
+    # Example: Use a shorter, hashed version of the trial name
+    trial_name = str(trial.trial_id)
+    return os.path.join("ray_results", trial_name)
+
+# Assuming your objective function and config are defined
+analysis = tune.run(
     objective,
     config=space,
-    num_samples=100, # Number of trials to run
-    metric="loss", # The name of the metric to optimize
-    mode="min", # Minimize the loss
+    num_samples=100,
+    metric="loss",
+    mode="min",
+    trial_dirname_creator=short_trial_dirname_creator
 )
 
 # Get the best parameters
