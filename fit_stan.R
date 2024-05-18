a687b9a40621916679f742055869583303069b18
AnaZonia <aavila@mail.mcgill.ca>
2024-05-18 16:50:02 -0400

    fixing fallow for all land use histories

--- fit_stan.R
+++ fit_stan.R
@@ -7,12 +7,14 @@ data <- read.csv('./data/amazon_df_sample_10million.csv')
 tst <- aggregate(age~agbd, data, median)
 
 
-data = list(df=tst, predictors=ncol(tst), pixels=nrow(tst))
+data = list(ages=tst$age, agbds=tst$agbd, n=nrow(tst))
 
 iter = 2000
 warmup=1000
 chains=2
 
-
-fit.stan <- stan(file=model.stan, data=data,iter=iter, warmup = warmup, chains=chains, cores=3,
-                 init = list(list()))
+fit.stan <- stan(file="model.stan", data = data, iter = iter, warmup = warmup,
+                 chains = chains, cores = 3,
+                 init = list(list(age = 1, theta = 0.5, A = 80, B0 = 5),
+                             list(age = 0.01, theta = 1, A = 100, B0 = 1)),
+                 control=list(max_treedepth=12))
