6273e6d518ebcf02c495e5f8b69310f1cdcdea29
AnaZonia <aavila@mail.mcgill.ca>
2024-05-24 10:53:39 -0400

    split python script

--- fit_scipy.py
+++ fit_scipy.py
@@ -15,8 +15,6 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 from scipy.stats import norm
-import ray
-from ray import tune
 import matplotlib.pyplot as plt
 import seaborn as sns
 
@@ -90,48 +88,6 @@ x0 = list(pars.values())
 # Now you can call minimize with the modified growth_curve function
 result = minimize(negative_log_likelihood, x0, args=(data,), method='Nelder-Mead')
 
-
-
-space = {
-    'B0': tune.uniform(0, 100),
-    'A': tune.uniform(0, 500),
-    'theta': tune.uniform(0, 1),
-    'sd': tune.uniform(0, 1),
-}
-
-space.update({key: tune.uniform(0, 100) for key in pars})
-
-# Initialize Ray
-ray.init()
-
-# Define the objective function for Ray Tune
-def objective(config):
-    return negative_log_likelihood(config, data)
-
-# def run_model(path):
-#     data = import_data(path)  # Replace "path_to_your_data.csv" with the actual path to your CSV file
-
-def short_trial_dirname_creator(trial):
-    # Example: Use a shorter, hashed version of the trial name
-    trial_name = str(trial.trial_id)
-    return os.path.join("ray_results", trial_name)
-
-# Assuming your objective function and config are defined
-analysis = tune.run(
-    objective,
-    config=space,
-    num_samples=100,
-    metric="loss",
-    mode="min",
-    trial_dirname_creator=short_trial_dirname_creator
-)
-
-# Get the best parameters
-best_trial = analysis.get_best_trial("loss", "min", "last")
-best_params = best_trial.config
-
-# print("Best parameters:", best_params)
-# print("Best loss:", best_trial.last_result["loss"])
 
 # if __name__=="__main__":
 #     path = './data/final_w_biomass.csv'
