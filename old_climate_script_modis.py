9980920e325a96834ac5bb897ad3e1729d83d4b4
AnaZonia <aavila@mail.mcgill.ca>
2024-05-26 16:41:11 -0400

    began cleaning

--- old_climate_script_modis.py
+++ old_climate_script_modis.py
@@ -1,4 +1,3 @@
-
 # def prec_anomaly(month_prec):
 #     anom = month_prec.subtract(mean_prec).divide(sd_prec)
 #     drought_mask = anom.lt(0)
@@ -20,9 +19,6 @@
 # anom = prec.first().subtract(mean_prec).divide(sd_prec)
 # drought_mask = anom.lt(0)
 # plot = anom.updateMask(drought_mask)
-
-
-
 
 
 # years = ee.List.sequence(1985, 2019)
@@ -58,10 +54,6 @@
 # Map
 
 
-
-
-
-
 # MODIS Evapotranspiration
 
 # Calculate mean AET from MODIS and calculate CWD from the precipitation values as in Celso
@@ -74,44 +66,61 @@
 start = "2002-01-01"
 end = "2019-12-31"
 
-modis = ee.ImageCollection("MODIS/061/MOD16A2GF") \
-         .filterDate(start, end) \
-         .select('ET', 'ET_QC').map(lambda image : image.clip(roi)) \
-              .map(lambda image: image.reduceResolution(ee.Reducer.median(), bestEffort=True, maxPixels=1024) \
-                                       .reproject(crs='EPSG:4326', scale=10000))
+modis = (
+    ee.ImageCollection("MODIS/061/MOD16A2GF")
+    .filterDate(start, end)
+    .select("ET", "ET_QC")
+    .map(lambda image: image.clip(roi))
+    .map(
+        lambda image: image.reduceResolution(
+            ee.Reducer.median(), bestEffort=True, maxPixels=1024
+        ).reproject(crs="EPSG:4326", scale=10000)
+    )
+)
 
 
 # code sourced from https://spatialthoughts.com/2021/08/19/qa-bands-bitmasks-gee/
 def bitwise_extract(input, from_bit, to_bit):
-   mask_size = ee.Number(1).add(to_bit).subtract(from_bit)
-   mask = ee.Number(1).leftShift(mask_size).subtract(1)
-   return input.rightShift(from_bit).bitwiseAnd(mask)
+    mask_size = ee.Number(1).add(to_bit).subtract(from_bit)
+    mask = ee.Number(1).leftShift(mask_size).subtract(1)
+    return input.rightShift(from_bit).bitwiseAnd(mask)
 
-def apply_QA_mask (image):
-    QA = image.select('ET_QC')
-    ET = image.select('ET').multiply(0.0125)  # multiply by the scale 0.1, divide by 8 to get daily info
+
+def apply_QA_mask(image):
+    QA = image.select("ET_QC")
+    ET = image.select("ET").multiply(
+        0.0125
+    )  # multiply by the scale 0.1, divide by 8 to get daily info
     cloud_mask = bitwise_extract(QA, 3, 4).lte(0)
     qa_mask = bitwise_extract(QA, 5, 7).lte(1)
     mask = cloud_mask.And(qa_mask)
-    return ET.updateMask(mask).set('system:time_start', image.get('system:time_start'))
+    return ET.updateMask(mask).set("system:time_start", image.get("system:time_start"))
+
 
 # mask quality of pixels
-modis_masked = modis.map(apply_QA_mask).map(lambda image:image.updateMask(mature_mask))
+modis_masked = modis.map(apply_QA_mask).map(lambda image: image.updateMask(mature_mask))
+
 
 # Loop through the months and filter images
 def monthly_et(month):
-    eight_day_images = modis_masked.select('ET').filter(ee.Filter.calendarRange(month, month, 'month'))
-    month_et = eight_day_images.median().multiply(30).reduceRegions(ecoregions, \
-                                                                      reducer = ee.Reducer.median(), scale = 10000)
-    month_et = month_et.reduceToImage(['month_et'], ee.Reducer.first())
-    return month_et.set('month', month)
+    eight_day_images = modis_masked.select("ET").filter(
+        ee.Filter.calendarRange(month, month, "month")
+    )
+    month_et = (
+        eight_day_images.median()
+        .multiply(30)
+        .reduceRegions(ecoregions, reducer=ee.Reducer.median(), scale=10000)
+    )
+    month_et = month_et.reduceToImage(["month_et"], ee.Reducer.first())
+    return month_et.set("month", month)
+
 
 monthly_et_imgcol = ee.ImageCollection.fromImages(months.map(monthly_et))
 
-yearlist = range(2002, 2019) # Generate a list of years from 1985 to 2019
+yearlist = range(2002, 2019)  # Generate a list of years from 1985 to 2019
 monthlist = range(1, 12)
 new_band_names = []
 for year in yearlist:
     for month in monthlist:
-        new_band_names = [f'mcwd_{month}_{year}']
+        new_band_names = [f"mcwd_{month}_{year}"]
 monthly_et_img = monthly_et_imgcol.toBands().rename(new_band_names)
--- ray_hyperparameter_tune.py
+++ ray_hyperparameter_tune.py
@@ -1,8 +1,8 @@
 space = {
-    'B0': tune.uniform(0, 100),
-    'A': tune.uniform(0, 500),
-    'theta': tune.uniform(0, 1),
-    'sd': tune.uniform(0, 1),
+    "B0": tune.uniform(0, 100),
+    "A": tune.uniform(0, 500),
+    "theta": tune.uniform(0, 1),
+    "sd": tune.uniform(0, 1),
 }
 
 space.update({key: tune.uniform(0, 100) for key in pars})
@@ -10,17 +10,21 @@ space.update({key: tune.uniform(0, 100) for key in pars})
 # Initialize Ray
 ray.init()
 
+
 # Define the objective function for Ray Tune
 def objective(config):
     return negative_log_likelihood(config, data)
 
+
 # def run_model(path):
 #     data = import_data(path)  # Replace "path_to_your_data.csv" with the actual path to your CSV file
+
 
 def short_trial_dirname_creator(trial):
     # Example: Use a shorter, hashed version of the trial name
     trial_name = str(trial.trial_id)
     return os.path.join("ray_results", trial_name)
+
 
 # Assuming your objective function and config are defined
 analysis = tune.run(
@@ -29,7 +33,7 @@ analysis = tune.run(
     num_samples=100,
     metric="loss",
     mode="min",
-    trial_dirname_creator=short_trial_dirname_creator
+    trial_dirname_creator=short_trial_dirname_creator,
 )
 
 # Get the best parameters
