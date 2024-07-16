e6120b09448281a536b7410bf3c3f75b4220b14c
AnaZonia <aavila@mail.mcgill.ca>
2024-07-16 14:11:54 -0400

    fitting optim

--- fit_optim.r
+++ fit_optim.r
@@ -22,11 +22,11 @@ library(doParallel)
 
 run_all <- TRUE
 run_one <- TRUE
-include_k_par <- TRUE
+include_k_par <- FALSE
 
 #------------------ GLobal Variables ------------------#
 
-CLIMATIC_VARS <- c("prec", "si")
+climatic_vars <- c("prec", "si")
 
 #------------------ FUNCTIONS ------------------#
 
@@ -39,7 +39,7 @@ CLIMATIC_VARS <- c("prec", "si")
 
 import_data <- function(path) {
   data <- read_csv(path) %>%
-    select(-c(.geo, latitude, longitude, biome)) %>%
+    select(-c(.geo, latitude, longitude)) %>%
     select(-starts_with("system"))
 
   # Convert specified variables to factors
@@ -48,23 +48,24 @@ import_data <- function(path) {
   data <- data %>%
     mutate(across(all_of(categorical), as.factor))
   
-  data <- data %>%
-    group_by(ecoreg) %>%
-    mutate(
-      mean_per_ecoregion = mean(mature_biomass, na.rm = TRUE),
-      sd_per_ecoregion = sd(mature_biomass, na.rm = TRUE)
-    ) %>%
-    ungroup()
+  # data <- data %>%
+  #   group_by(ecoreg) %>%
+  #   mutate(
+  #     mean_per_ecoregion = mean(mature_biomass, na.rm = TRUE),
+  #     sd_per_ecoregion = sd(mature_biomass, na.rm = TRUE)
+  #   ) %>%
+  #   ungroup()
 
-  data <- data %>%
-    filter(
-      mature_biomass > agbd,
-      mature_biomass < (mean_per_ecoregion + sd_per_ecoregion),
-      mature_biomass > (mean_per_ecoregion - sd_per_ecoregion)
-    )
+  # data <- data %>%
+  #   filter(
+  #     mature_biomass > agbd,
+  #     mature_biomass < (mean_per_ecoregion + sd_per_ecoregion),
+  #     mature_biomass > (mean_per_ecoregion - sd_per_ecoregion)
+  #   )
 
   # Create dummy variables
   data <- dummy_cols(data, select_columns = categorical, remove_selected_columns = TRUE)
+  data <- data %>% filter(!is.na(mature_biomass))
 
   data
 }
@@ -72,21 +73,21 @@ import_data <- function(path) {
 import_climatic_data <- function(path, normalize) {
   data <- import_data(path)
 
-  means <- sapply(CLIMATIC_VARS, function(var) rowMeans(data[, grep(var, names(data))], na.rm = TRUE))
-  colnames(means) <- paste0("mean_", CLIMATIC_VARS)
+  means <- sapply(climatic_vars, function(var) rowMeans(data[, grep(var, names(data))], na.rm = TRUE))
+  colnames(means) <- paste0("mean_", climatic_vars)
   data <- cbind(data, means)
 
   df_climatic_hist <- tibble()
   for (age in 1:max(data$age)) {
     age_data <- data %>% filter(age == .env$age)
     years <- seq(2019, 2019 - age + 1, by = -1)
-    # Identify all columns including the variables in CLIMATIC_VARS
-    clim_columns <- expand.grid(CLIMATIC_VARS, years) %>%
+    # Identify all columns including the variables in climatic_vars
+    clim_columns <- expand.grid(climatic_vars, years) %>%
       unite(col = "col", sep = "_") %>%
       pull(col)
 
     # subsect the dataframe to only include the climatic columns of the desired years
-    all_clim_columns <- names(data)[str_detect(names(data), paste(CLIMATIC_VARS, "_", collapse = "|"))]
+    all_clim_columns <- names(data)[str_detect(names(data), paste(climatic_vars, "_", collapse = "|"))]
 
     # turn all values in the columns of years not included to 0
     clim_columns_not_included <- setdiff(all_clim_columns, clim_columns)
@@ -99,7 +100,7 @@ import_climatic_data <- function(path, normalize) {
     df_climatic_hist <- df_climatic_hist %>%
       mutate(across(
         where(is.numeric) &
-          !matches("soil|ecoreg|last_LU|protec|indig|agbd|mat_gaus_ker|mature_biomass|mean_per_ecoregion|sd_per_ecoregion"),
+          !matches("soil|biome|ecoreg|last_LU|protec|indig|agbd|mature_biomass|mean_per_ecoregion|sd_per_ecoregion"),
         ~ (. - min(., na.rm = TRUE)) / (max(., na.rm = TRUE) - min(., na.rm = TRUE))
       )) %>%
       select(where(~ sum(is.na(.)) < nrow(df_climatic_hist)))
@@ -123,20 +124,25 @@ growth_curve <- function(pars, data, pars_chosen) {
     k <- k + pars[["k"]]
   }
 
-  non_clim_vars <- setdiff(pars_chosen, CLIMATIC_VARS)
+  non_clim_vars <- setdiff(pars_chosen, climatic_vars)
 
-  if (any(CLIMATIC_VARS %in% pars_chosen)) {
-      k <- k + rowSums(sapply(non_clim_vars, function(var) pars[[var]] * data[[var]] * data[["age"]]))
-
-    for (clim_var in intersect(CLIMATIC_VARS, pars_chosen)) {
-      years <- seq(2019, 1985, by = -1)
-      clim_columns <- paste0(clim_var, "_", years)
-
-      k <- k + rowSums(sapply(clim_columns, function(col) pars[[clim_var]] * data[[col]]))
-    }
-  } else {
-    k <- k + rowSums(sapply(non_clim_vars, function(var) pars[[var]] * data[[var]]))
+  tmp_age <- 1
+  tmp_age1 <- data[[age]]
+  if ("age" %in% pars_chosen)
+  {
+    tmp_age <- data[["age"]]
+    tmp_age1 <- 1
   }
+
+  #if (any(climatic_vars %in% pars_chosen)) {
+  k <- k + rowSums(sapply(non_clim_vars, function(var) pars[[var]] * data[[var]] * tmp_age))
+
+  for (clim_var in intersect(climatic_vars, pars_chosen)) {
+    years <- seq(2019, 1985, by = -1)
+    clim_columns <- paste0(clim_var, "_", years)
+    k <- k + rowSums(sapply(clim_columns, function(col) pars[[clim_var]] * data[[col]/tmp_age1]))
+  }
+
 
   pars[["B0"]] + (data[["mature_biomass"]] - pars[["B0"]]) * (1 - exp(-k))^pars[["theta"]]
 }
@@ -209,19 +215,21 @@ run_optimization <- function(fun, pars_basic, data, pars_chosen, conditions) {
 ################### Running model ###################
 
 datafiles <- list(
-  "data/5y_LULC.csv",
-  "data/10y_LULC.csv",
-  "data/15y_LULC.csv",
-  "data/all_LULC.csv"
+  # "data/5y_LULC.csv",
+  "data/10y_LULC_1km.csv" # "data/15y_LULC_1km.csv",
+  # "data/all_LULC.csv"
 )
 
-climatic_vars <- c("prec", "si")
-dataframes <- lapply(datafiles, import_climatic_data, normalize = FALSE)
+
+
+dataframes <- lapply(datafiles, import_climatic_data, normalize = TRUE)
+
 # adding names can make indexing complicated, so they are being referenced as a vector
-names_dataframes <- c("data_5", "data_10", "data_15", "data_all")
-dataframes <- lapply(dataframes, function(df) {
-  df %>% filter(!is.na(mature_biomass))
-})
+# names_dataframes <- c("data_5", "data_10", "data_15", "data_all")
+# dataframes <- lapply(dataframes, function(df) {
+#   df %>% filter(!is.na(mature_biomass))
+# })
+
 
 if (run_all || run_one) {
   # Define conditions
@@ -241,7 +249,7 @@ if (run_all || run_one) {
   colnames_intersect <- Reduce(intersect, map(dataframes, colnames))
   # also exclude columns that are not predictors
   colnames_filtered <- colnames_intersect[!grepl(
-    "agbd|latitude|longitude|prec|si|mat_gaus_ker|mature_biomass",
+    "agbd|latitude|longitude|prec|si|mature_biomass",
     colnames_intersect
   )]
 
@@ -249,7 +257,7 @@ if (run_all || run_one) {
     c("age"),
     c("num_fires_before_regrowth"),
     c("age", "num_fires_before_regrowth"),
-    c("age", "num_fires_before_regrowth", "sur_cover", "fallow", "indig", "protec", "cwd"),
+    c("indig", "protec", "cwd", "mean_prec", "mean_si", names(data)[str_detect(names(data), "LU")]),
     colnames_filtered,
     c(colnames_filtered, climatic_vars)
   )
@@ -260,9 +268,12 @@ if (run_all || run_one) {
 
 
 if (run_one) {
-  data <- dataframes[[3]]
-
-  pars_chosen <- configurations[[3]]
+  data <- dataframes[[1]]
+  data <- data %>%
+    filter(
+      mature_biomass > agbd
+    )
+  pars_chosen <- configurations[[4]]
   val <- run_optimization(
     "nls", pars_basic, data, pars_chosen,
     if ("age" %in% pars_chosen) {
@@ -309,8 +320,9 @@ if (run_all) {
   # print(min(unlist(sum_squares_fit)))
 }
 
-
-
+data1 <- dataframes[[1]] %>% filter(biome == 1)
+plot(agbd ~ mature_biomass, data = data1, col = "blue")
+abline(0, 1, col = "red")
 
 run_growth_model <- function(data, initial_pars) {
   conditions <- list(
@@ -363,7 +375,7 @@ run_growth_model <- function(data, initial_pars) {
 # Usage
 for (i in seq_along(dataframes)) {
   print("----------------------------------------------------")
-  print(names_dataframes[i])
+  # print(names_dataframes[i])
 
   # Without asymptote (using mature_biomass)
   result1 <- run_growth_model(dataframes[[i]], c(B0 = 40, theta = 5, k = 0.1))
@@ -376,27 +388,72 @@ for (i in seq_along(dataframes)) {
 
 
 
-
-
-
-
-
-
-
-
-
 library(mgcv)
 library(randomForest)
 
+data <- dataframes[[1]]
+# data <- read.csv("data/10y_LULC_1km.csv")
+# data <- data %>% filter(!is.na(mature_biomass))
+# nrow(data)
+# mean(data$agbd)
+# mean(data$mature_biomass)
 
+# lulc_columns <- names(data)[str_detect(names(data), "lulc")]
+# lulc_unique_values <- lapply(lulc_columns, function(col) unique(data[[col]]))
+# names(lulc_unique_values) <- lulc_columns
+# # Print the results
+# for (col in lulc_columns) {
+#   cat(col, ":\n")
+#   print(lulc_unique_values[[col]])
+#   cat("\n")
+# }
 # Split data into training and testing sets
+
 set.seed(123)
 train_indices <- sample(1:nrow(data), size = floor(0.7 * nrow(data)))
 train_data <- data[train_indices, ]
 test_data <- data[-train_indices, ]
 
+
+lulc_sum_columns <- names(data)[str_detect(names(data), "lulc")]
+lulc_sum_columns <- lulc_sum_columns[!grepl("lulc_sum_(48|47|40)$", lulc_sum_columns)]
+
+pars_categ <- c("indig", "protec", names(data)[str_detect(names(data), "LU")])
+# pars_categ <- c()
+pars_smooth <- c("mean_si", "mean_prec", "cwd", "mature_biomass"
+# , lulc_sum_columns # "lulc_sum_15"
+)
+
+# Fit a GAM model
+formula <- as.formula(paste(
+  "agbd ~",
+  paste(sapply(pars_smooth, function(x) paste0("s(", x, ")")), collapse = " + "),
+  "+",
+  paste(pars_categ, collapse = " + ")
+))
+
+gam_model <- gam(formula, data = train_data)
+summary(gam_model)
+
+# Predict using the GAM model
+pred_gam <- predict(gam_model, newdata = test_data)
+cor(test_data$agbd, pred_gam)^2
+
+# Plot predictions vs observed
+plot(pred_gam, data$agbd)
+abline(0, 1, col = "red")
+
+rf_pars <- c(pars_smooth, pars_categ, "age")
+rf_pars
+
+rf_formula <- as.formula(paste("agbd ~", paste(rf_pars, collapse = " + ")))
+summary(lm(rf_formula, data = data))
+summary(lm(agbd ~ mature_biomass, data = data))
+
+train_data <- na.omit(train_data)
+
 # Fit a Random Forest model on the training data
-rf_model <- randomForest(agbd ~ ., data = train_data, ntree = 500, mtry = sqrt(ncol(train_data) - 1), importance = TRUE, keep.forest = TRUE, oob.score = TRUE)
+rf_model <- randomForest(rf_formula, data = train_data, ntree = 500, mtry = sqrt(ncol(train_data) - 1), importance = TRUE, keep.forest = TRUE, oob.score = TRUE)
 
 # Print model summary
 print(rf_model)
@@ -411,30 +468,10 @@ plot(pred_rf_test, test_data$agbd)
 abline(0, 1, col = "red")
 
 
-data <- dataframes[[1]]
 
+data <- read.csv("./data/mat_for_biomass_and_distance.csv")
+data <- data[, 2:3]
 
-pars_categ <- c("indig", "protec")
-
-pars_smooth <- c("mean_si", "mean_prec", "cwd") # , "num_fires_before_regrowth", "cwd", "fallow", "age", "sur_cover", "lulc_sum_15")
-# names(data)[str_detect(names(data), "LU|lulc")])
-
-# Fit a GAM model
-# Construct the formula dynamically
-formula <- as.formula(paste(
-  "agbd ~",
-  paste(sapply(pars_smooth, function(x) paste0("s(", x, ")")), collapse = " + "),
-  "+",
-  paste(pars_categ, collapse = " + ")
-))
-
-# Fit the GAM model with the dynamically created formula
-gam_model <- gam(formula, data = data)
-summary(gam_model)
-
-# Predict using the GAM model
-pred_gam <- predict(gam_model, newdata = data)
-
-# Plot predictions vs observed
-plot(pred_gam, data$agbd)
-abline(0, 1, col = "red")
+# why in here it is showing only a few points so far away? makes no sense.
+data <- subset(data, distance < 5000)
+plot(data$distance, data$mature_biomass)
