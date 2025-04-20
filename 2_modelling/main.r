f027faa41fdc362b76fc5c2dfc4974d336665ae0
AnaZonia <ana.vitorino@mail.mcgill.ca>
2025-04-20 17:13:04 -0400

    realized that optim_ga is leading to the same issues with theta as seen before - introducing theta will still lead to a local minima, I think the ridge is there no matter tha algorithm and optim_ga does not help much beyond what the initial optim/feature selection can provide for lag/k0/B0. For now moving on without it and with theta=1, and later will come back to it if needed.

--- 2_modelling/main.r
+++ 2_modelling/main.r
@@ -21,10 +21,42 @@ n_samples <- 10000
 
 # Set up parallel processing
 set.seed(1)
-registerDoParallel(cores = 4)
+ncore = 4
+registerDoParallel(cores = ncore)
 
 # Load data
+biome = 1
+n_samples = 10000
 data <- import_data("./0_data/unified_fc.csv", biome = biome, n_samples = n_samples)
+
+# Fit the model on the full data
+norm_data <- normalize_independently(data)$train_data
+
+# OPTIM GA ----------------------------------------------------------------------
+
+# ini_par <- data.frame(B0 = 80, k0 = 1, theta = 2)
+
+# for (j in 2:ncore){
+#     ini_par[j, ] <- c(
+#         ini_par[1, "B0"] * (1.5 * runif(1) + .5),
+#         ini_par[1, "k0"] * (1.5 * runif(1) + .5),
+#         ini_par[1, "theta"] * (1.5 * runif(1) + 0.5)
+#     )
+# }
+
+# conditions <- list('pars[["theta"]] > 10', 'pars[["theta"]] < 0', 'pars[["k0"]] < 0')
+
+# optim_ga(ini_par, norm_data)
+
+# --------------------------------------------------------------------------------
+
+pars_init <- find_combination_pars(basic_pars = basic_pars_options[["lag"]], "data_pars" = data_pars_options(colnames(data))[["land_use_landscape_only"]], norm_data)
+
+final_model <- run_optim(norm_data, pars_init, conditions)
+
+
+
+
 
 # Function to run a single experiment
 run_experiment <- function(basic_pars_name, data_pars_name, biome) {
@@ -67,73 +99,8 @@ for (name in names(data_pars_options(colnames(data)))) {
 # just vary basic_pars
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
-
 for (name in names(basic_pars_options)) {
     result <- run_experiment(name, "all_mean_climate", 1)
     print(result)
     results <- rbind(result)
 }
-
-# write.csv(results, "0_results/model_comparisons.csv")
-
-
-# get data for plotting
-source("2_R_scripts/1_modelling.r")
-# Load data
-data <- import_data("./0_data/unified_fc.csv", biome = biome, n_samples = n_samples)
-
-# Fit the model on the full data
-norm_data <- normalize_independently(data)$train_data
-pars_init <- find_combination_pars(basic_pars = basic_pars_options[["lag"]], "data_pars" = data_pars_options(colnames(data))[["land_use_landscape_only"]], norm_data)
-
-final_model <- run_optim(norm_data, pars_init, conditions)
-
-
-data[["pred_lagged"]] <- growth_curve(final_model$par, norm_data, exp(final_model$par[["lag"]]))
-data[["pred_nolag"]] <- growth_curve(final_model$par, norm_data) # with no lag, to give the expected values at low ages
-data[["pred_future"]] <- growth_curve(final_model$par, norm_data, exp(final_model$par[["lag"]])+35) # with no lag, to give the expected values at low ages
-
-print(mean(data[["pred_lagged"]]))
-print(mean(data[["pred_nolag"]]))
-print(mean(data[["pred_future"]]))
-
-
-
-write.csv(data, paste0(c("0_results/lagged_nolag_unified_data.csv"), collapse = "_"), row.names = FALSE)
-
-
-
-# --------------------------------- Plotting ---------------------------------#
-
-# get data for plotting
-source("2_R_scripts/1_modelling.r")
-source("2_R_scripts/1_data_processing.r")
-source("2_R_scripts/1_parameters.r")
-
-biome <- 1
-n_samples <- 10000
-# Load data
-
-data <- import_data("./0_data/unified_fc_old_biomass.csv", biome = biome, n_samples = n_samples) %>%
-    rename(biomass = b1) %>%
-    # remove mean_pdsi column
-    select(-c(mean_pdsi))
-
-options <- data_pars_options(colnames(data))
-options
-
-
-norm <- normalize_independently(data)
-norm_data <- norm$train_data
-norm_stats <- norm$train_stats
-
-pars_init <- find_combination_pars(basic_pars = c("B0", "k0", "theta"), "data_pars" = options[["all_no_categorical"]], norm_data)
-pars_init
-
-
-final_model <- run_optim(norm_data, pars_init, conditions)
-final_model$par
-
-
-
-permutation_importance <- calculate_permutation_importance(final_model, norm_data, options[["all_no_categorical"]])
